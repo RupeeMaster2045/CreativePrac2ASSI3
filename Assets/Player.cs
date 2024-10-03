27f822a10ed40843eab933e7038e0abae3cdeb51
RupeeMaster <link28119@gmail.com>
2024-10-03 13:44:53 +1000

    jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,18 +7,47 @@ public class Player : MonoBehaviour
 
     public float speed;
     public float jump;
+    public float groundedY;
 
-    // Update is called once per frame
     void Update()
     {
-       // Move
+
+        // Move
         transform.Translate(Vector2.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
 
         // Jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
+
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+
         }
-        
+
     }
+
+    public bool IsGrounded()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, groundedY), Vector2.down, .1f);
+
+        if (hit.collider != null)
+        {
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawRay(transform.position + new Vector3(0, groundedY), Vector2.down * .1f);
+
+    }
+
 }
